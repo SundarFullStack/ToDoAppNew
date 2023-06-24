b74196d31052403c614375afde65a9bd5ab91e11
Sundar Thambidurai <meenakshisunder183017@gmail.com>
2023-06-24 21:01:45 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Pricecard from './components/Pricecard/Pricecard';
-import ReactForm from './components/ReactForm/ReactForm';
+
 
 function App() {
   return (
     <div className="App">
   
-{/* 
-      <Pricecard/> */}
 
-      <ReactForm/>
+      <Pricecard/>
+
+      {/* <ReactForm/> */}
  
      
     </div>
